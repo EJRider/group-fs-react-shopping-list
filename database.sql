0a37199f3e2afcae8602260521408d83a0fc8269
Rider <goejrider@gmail.com>
2022-10-20 09:35:05 -0500

    fix decimal

--- database.sql
+++ database.sql
@@ -1,3 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- Make database react-shopping-list
 -- It is also helpful to include some test data
+
+CREATE TABLE "list" (
+	"id" SERIAL PRIMARY KEY,
+	"item" VARCHAR (80) NOT NULL,
+	"quantity" INTEGER NOT NULL,
+	"unit" VARCHAR (20),
+  	"purchased" BOOLEAN DEFAULT FALSE
+);
+
