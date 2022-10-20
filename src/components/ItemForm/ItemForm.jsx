b59d76c8becf6ee18610be69e49d857538a53c95
Crystal Redmon <cato2848@gmail.com>
2022-10-20 16:05:52 -0500

    complete item form'

---
+++ src/components/ItemForm/ItemForm.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+function ItemForm({ addItem }) {
+
+    let [newItemName, setItemName] = useState('');
+    let [itemQuantity, setItemQuantity] = useState(0);
+    let [itemUnit, setItemUnit] = useState(0);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        let newItem = {
+            item: newItemName,
+            quantity: itemQuantity,
+            unit: itemUnit
+        }
+
+        console.log(newItem)
+
+        if(newItemName){
+
+            addItem(newItem);
+
+            setItemName('');
+            setItemQuantity(0);
+            setItemUnit('0');
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+    return (<>
+
+
+        <form onSubmit= {handleSubmit}>
+            <h2>Add Item</h2>
+            <input 
+            type="text"
+            placeholder="Item"
+            required
+            value={newItemName}
+            onChange={(evt)=> setItemName(evt.target.value) }
+            ></input>
+
+
+            <input 
+            type="number"
+            placeholder="Quantity"
+            required
+            value={itemQuantity}
+            onChange={(evt)=> setItemQuantity(evt.target.value)}
+            ></input>
+
+
+
+            <input 
+            placeholder="Unit"
+            value={itemUnit}
+            onChange={(evt) => setItemUnit(evt.target.value)}
+            
+            ></input>
+
+
+            <button>Submit</button>
+        </form>
+
+
+
+
+    </>)
+}
+
+
+export default ItemForm;
