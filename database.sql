c5b9536b7db179dde0d057e2b76785147396462e
Rider <goejrider@gmail.com>
2022-10-20 09:37:17 -0500

    Add exmaple items for database

--- database.sql
+++ database.sql
@@ -5,8 +5,17 @@
 CREATE TABLE "list" (
 	"id" SERIAL PRIMARY KEY,
 	"item" VARCHAR (80) NOT NULL,
-	"quantity" INTEGER NOT NULL,
+	"quantity" DECIMAL NOT NULL,
 	"unit" VARCHAR (20),
   	"purchased" BOOLEAN DEFAULT FALSE
 );
 
+INSERT INTO "list"
+	("item","quantity","unit")
+VALUES
+	('Bread','1.5','loafs');
+
+INSERT INTO "list"
+    ("item","quantity","unit")
+VALUES
+    ('Eggs', '2', 'cartons');
